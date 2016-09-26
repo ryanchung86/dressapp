b1bc5a5685a32ba6f6241b13a7caf8995eb49026
Ryan <ryan.y.chung@gmail.com>
2016-09-26 16:42:49 -0400

    set up suitcases controller

--- dressapp/app/controllers/suitcases_controller.rb
+++ dressapp/app/controllers/suitcases_controller.rb
@@ -1,19 +1,38 @@
 class SuitcasesController < ApplicationController
   def index
+    @suitcases = Suitcase.all
   end
 
   def show
+    @suitcase = Suitcase.find(params[:id])
   end
 
   def edit
+    @suitcase = Suitcase.find(params[:id])
   end
 
   def new
+    @suitcase = Suitcase.new
   end
 
   def create
+    @suitcase = Suitcase.new
+
+    if @suitcase.save
+      redirect_to suitcases_url
+    else
+      render :new
+    end
   end
 
   def destroy
+    @suitcase = Suitcase.find(params[:id])
+    @suitcase.destroy
+    redirect_to suitcases_url
+  end
+
+  private
+  def suitcase_params
+    params.require(:suitcase).permit(:name, :description, :duration, :destination)
   end
 end
